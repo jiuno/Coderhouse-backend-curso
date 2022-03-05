62a3d7d0eb477832c38a13bbd0cb957e10cf6ba1
Jiuno <bruno.alliani@gmail.com>
2022-03-05 11:46:33 -0300

    Complete Challenge 2

---
+++ Clase 2/Desafios en clase/funcionesYClosures.js
@@ -0,0 +1,43 @@
+/* 01 - Definir la función mostrarLista que reciba una lista de datos y muestre su
+contenido, si no está vacía, o de lo contrario muestre el mensaje: “lista vacía”.
+Luego, invocarla con datos de prueba para verificar que funciona bien en ambos
+casos.*/
+
+
+/*function mostrarLista(lista) {
+    if (lista.length > 0) {
+        console.log(lista);
+        
+    } else {
+        console.log("lista vacía")
+    };
+    
+};
+
+const a = [1,2,3]
+const b = []
+const c = [1,"hola",true]
+
+mostrarLista(a)
+
+mostrarLista(b)
+
+mostrarLista(c)*/
+
+
+/* 02 - Definir una función anónima que haga lo mismo que la del punto 1, e invocarla
+inmediatamente, pasando una lista con 3 números como argumento.*/
+
+/*Pregunta: Por que si descomento el ejercicio anterior arroja el error "TypeError: mostrarLista(...) is not a function"
+(function(){ console.log([1,2,3])})(); */
+
+/* 03 - Definir la función crearMultiplicador que reciba un número y devuelva una
+función anónima que reciba segundo número y dé como resultado el producto de
+ambos. Luego, a partir de la función definida, crear dos funciones duplicar y
+triplicar, y probarlas con diferentes valores.*/
+
+
+
+
+
+
