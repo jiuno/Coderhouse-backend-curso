c906c2b7226f478e0c8b2220d10068278a795b62
Jiuno <bruno.alliani@gmail.com>
2022-03-05 11:10:14 -0300

    Add first Challenge.

---
+++ Clase 2/Desafios en clase/funcionesYClosures01.js
@@ -0,0 +1,5 @@
+/*Definir la función mostrarLista que reciba una lista de datos y muestre su
+contenido, si no está vacía, o de lo contrario muestre el mensaje: “lista vacía”.
+Luego, invocarla con datos de prueba para verificar que funciona bien en ambos
+casos.*/
+
