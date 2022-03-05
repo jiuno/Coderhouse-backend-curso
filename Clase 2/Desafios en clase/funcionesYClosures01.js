1d0fd7ff40458895189061b0fa13fa747d5b1a65
Jiuno <bruno.alliani@gmail.com>
2022-03-05 11:18:35 -0300

    Complete Challenge.

--- Clase 2/Desafios en clase/funcionesYClosures01.js
+++ Clase 2/Desafios en clase/funcionesYClosures01.js
@@ -3,3 +3,22 @@ contenido, si no está vacía, o de lo contrario muestre el mensaje: “lista va
 Luego, invocarla con datos de prueba para verificar que funciona bien en ambos
 casos.*/
 
+function mostrarLista(lista) {
+    if (lista.length > 0) {
+        console.log(lista);
+        
+    } else {
+        console.log("lista vacía")
+    };
+    
+};
+
+const a = [1,2,3]
+const b = []
+const c = [1,"hola",true]
+
+mostrarLista(a)
+
+mostrarLista(b)
+
+mostrarLista(c)
