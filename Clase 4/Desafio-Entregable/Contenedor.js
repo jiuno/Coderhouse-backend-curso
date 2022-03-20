56cb98f956a8fb63ef138c9c1b3de4db1ffd85d4
Jiuno <bruno.alliani@gmail.com>
2022-03-20 18:48:30 -0300

    Lectura JSON sync

--- Clase 4/Desafio-Entregable/Contenedor.js
+++ Clase 4/Desafio-Entregable/Contenedor.js
@@ -1,23 +1,59 @@
 const fs = require('fs')
 
+productos = []
+
 class Contenedor {
   archivo //Archivo donde se guardaran los productos
   constructor(archivo) {
     this.archivo = archivo
-    this.productos = []
     this.id = 0
   }
 
-  async leerAsync() {
+  escribir(data) {
     try {
-      const dataRaw = await fs.promises.readFile(this.archivo)
-      console.log(dataRaw[1])
+      fs.writeFileSync(this.archivo, JSON.stringify(data, null, '\t'))
     } catch (error) {
-      console.log('Error de lectura', error)
+      console.log('Error de escritura: ', error)
     }
+  }
+
+  leer() {
+    try {
+      const dataRaw = fs.readFileSync(this.archivo, 'utf-8')
+      return dataRaw
+    } catch (error) {
+      console.log('Error de lectura: ', error)
+    }
+  }
+
+  save(objeto) {
+    objeto.id = this.id++
+    productos.push(objeto)
+    this.escribir(productos)
+    return objeto.id
   }
 }
 
-const cont1 = new Contenedor('./productos.txt')
+const obj1 = {
+  title: 'lapiz',
+  price: 20,
+}
 
-cont1.leerAsync()
+const obj2 = {
+  title: 'goma',
+  price: 50,
+}
+
+const obj3 = {
+  title: 'regla',
+  price: 30,
+}
+
+const cont1 = new Contenedor('prueba1.txt')
+
+cont1.save(obj1)
+cont1.save(obj2)
+cont1.save(obj3)
+
+const datos = cont1.leer()
+console.log(JSON.parse(datos)[0])
