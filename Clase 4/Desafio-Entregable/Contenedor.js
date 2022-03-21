a38859a411053a1bfec206761d9a94a9a3d40b26
Jiuno <bruno.alliani@gmail.com>
2022-03-21 14:45:30 -0300

    prueba

--- Clase 4/Desafio-Entregable/Contenedor.js
+++ Clase 4/Desafio-Entregable/Contenedor.js
@@ -4,13 +4,39 @@ class Contenedor {
   fileName //[String] fileName donde se guardaran los objetos.
   constructor(fileName) {
     this.fileName = fileName
-    this.id = 1
+    this.id = 0
     this.objects = []
 
     const existeFile = fs.existsSync(this.fileName)
 
     if (existeFile) {
-      this.objects = this.leer()
+      this.leer()
+    }
+  }
+
+  leer() {
+    try {
+      const dataRaw = fs.readFileSync(this.fileName, 'utf-8')
+      this.objects = JSON.parse(dataRaw)
+      this.id = this.objects[this.objects.length - 1].id
+    } catch (error) {
+      console.log('Error de lectura: ', error)
+    }
+  }
+
+  async leerAsync() {
+    try {
+      const objects = await fs.promises.readFile(this.fileName, 'utf-8')
+      this.objects = JSON.parse(objects)
+    } catch (error) {
+      console.log('Error de lectura: ', error)
+    }
+  }
+  escribir(data) {
+    try {
+      fs.writeFileSync(this.fileName, JSON.stringify(data, null, '\t'))
+    } catch (error) {
+      console.log(error)
     }
   }
 
@@ -25,42 +51,59 @@ class Contenedor {
     }
   }
 
-  leer() {
-    try {
-      const dataRaw = fs.readFileSync(this.fileName, 'utf-8')
-      return JSON.parse(dataRaw)
-    } catch (error) {
-      console.log('Error de lectura: ', error)
-    }
-  }
-
-  async leerAsync() {
-    try {
-      const objects = await fs.promises.readFile(this.fileName, 'utf-8')
-      this.objects = JSON.parse(objects)
-    } catch (error) {
-      console.log('Error de lectura: ', error)
-    }
-  }
-
   save(object) {
-    object.id = this.id++
-    this.objects.push(object)
+    try {
+      this.id++
+      object.id = this.id
+      this.objects.push(object)
+      this.escribirAsync(this.objects)
+      return object.id
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async deleteAll() {
+    this.objects = []
     this.escribirAsync(this.objects)
-    return object.id
   }
-
   async getAll() {
-    const data = await this.leerAsync()
-    return data
+    try {
+      await this.leerAsync()
+      return this.objects
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  async getByID(ID) {
-    objects = await this.getAll()
-    for (let i = 0; i < objects.length; i++) {
-      if (objects[i].id == ID) {
-        return objects[ID]
+  async deleteByID(ID) {
+    let objs = await this.getByID()
+    console.log(objs)
+    const index = objs.findIndex((obj) => {
+      return obj.id == ID
+    })
+    objs.splice(index, 1)
+    console.log(objs)
+    // this.escribirAsync(objs)
+  }
+
+  getByID(ID) {
+    try {
+      let objEncontrado = null
+      const objects = this.getAll()
+      for (let i = 0; i < objects.length; i++) {
+        if (objects[i].id == ID) {
+          objEncontrado = objects[ID]
+        }
       }
+      if (objEncontrado) {
+        return objEncontrado
+      } else {
+        console.log('ID no encontrado')
+        return null
+      }
+    } catch (error) {
+      console.log(error)
     }
   }
 }
@@ -82,10 +125,9 @@ const obj3 = {
 
 const cont1 = new Contenedor('prueba1.txt')
 
-// cont1.save(obj1)
-// cont1.save(obj2)
+cont1.save(obj1)
+cont1.save(obj2)
+cont1.save(obj3)
+cont1.save(obj1)
+cont1.save(obj2)
 // cont1.save(obj3)
-
-const datos = cont1.getAll()
-
-console.log(datos)
