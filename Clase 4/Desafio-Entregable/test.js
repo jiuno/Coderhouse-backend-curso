378adf29798520502cd55f65211a7da5fd9b319e
Jiuno <bruno.alliani@gmail.com>
2022-03-22 20:22:32 -0300

    Agregue url a los objetos.

--- Clase 4/Desafio-Entregable/test.js
+++ Clase 4/Desafio-Entregable/test.js
@@ -3,16 +3,22 @@ const Contenedor = require('./Contenedor')
 const lapiz = {
   title: 'lapiz',
   price: 20,
+  url:
+    'https://www.istockphoto.com/es/vector/icono-del-l%C3%A1piz-plano-gm1078269892-288880222',
 }
 
 const goma = {
   title: 'goma',
   price: 30,
+  url:
+    'https://www.istockphoto.com/es/vector/borrador-de-ilustraci%C3%B3n-vectorial-gm180387767-27051280',
 }
 
 const regla = {
   title: 'regla',
   price: 10,
+  url:
+    'https://www.istockphoto.com/es/vector/colores-de-dibujos-animados-icono-cartel-gobernante-gm924517912-253722579',
 }
 
 const cont = new Contenedor('./productos.txt')
