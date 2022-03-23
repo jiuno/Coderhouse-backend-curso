42601665f3e7f9a18db1e623fea8fc7684f7bc99
Jiuno <bruno.alliani@gmail.com>
2022-03-23 13:03:38 -0300

    Desafio entregable 3 Terminado

---
+++ Clase 6/Desafio-Entregable/resources/Contenedor.js
@@ -0,0 +1,108 @@
+const fs = require('fs')
+
+class Contenedor {
+  fileName //[String] File where data is read and written.
+  constructor(fileName) {
+    this.fileName = fileName
+    this.id = 0
+    this.objects = []
+
+    if (fs.existsSync(this.fileName)) {
+      this.objects = this.readSync()
+      if (this.objects.length > 0) {
+        this.id = this.objects[this.objects.length - 1].id
+      }
+    }
+  }
+
+  async write(data) {
+    try {
+      return await fs.writeFileSync(
+        this.fileName,
+        JSON.stringify(data, null, '\t'),
+      )
+    } catch (err) {
+      console.log('Error en la escritura del archivo', err)
+    }
+  }
+
+  readSync() {
+    try {
+      const dataRaw = fs.readFileSync(this.fileName, 'utf-8')
+      return JSON.parse(dataRaw)
+    } catch (err) {
+      console.log(
+        'No existe el archivo. Guardar primero un objecto con Contenedor.save',
+        err,
+      )
+    }
+  }
+
+  async read() {
+    try {
+      const dataRaw = await fs.promises.readFile(this.fileName, 'utf-8')
+      return JSON.parse(dataRaw)
+    } catch (err) {
+      console.log(
+        'No existe el archivo. Guardar primero 1 objecto con Contenedor.save',
+        err,
+      )
+    }
+  }
+
+  async save(object) {
+    try {
+      let objCopy = Object.assign({}, object)
+      this.id++
+      objCopy.id = this.id
+      this.objects.push(objCopy)
+      await this.write(this.objects)
+      return objCopy.id
+    } catch (err) {
+      console.log('Error al guardar el objeto', err)
+    }
+  }
+
+  async getAll() {
+    try {
+      this.objects = await this.read()
+      return this.objects
+    } catch (err) {
+      console.log('No existe el archivo')
+    }
+  }
+
+  async getById(ID) {
+    try {
+      this.objects = await this.read()
+      const obj = this.objects.filter((ob) => {
+        return ob.id == ID
+      })
+      if (obj.length == 0) {
+        throw new Error('ID no encontrado')
+      } else if (obj.length > 1) {
+        throw new Error(`Mas de un objeto con el ID buscado. }`)
+      } else {
+        return obj[0] //Solo debe devolver 1 obj. El array no debe tener más de un elemento
+      }
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+
+  async deleteAll() {
+    this.objects = []
+    return await this.write(this.objects)
+  }
+
+  async deleteByID(ID) {
+    this.objects = await this.read()
+    const objsNotDeleted = this.objects.filter((ob) => {
+      return ob.id !== ID
+    })
+    this.objects = objsNotDeleted
+    await this.write(this.objects)
+  }
+}
+
+module.exports = Contenedor
---
+++ Clase 6/Desafio-Entregable/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const path = require('path')
+const ContenedorPath = path.resolve(__dirname, './resources/Contenedor.js')
+const resourcesPath = path.resolve(__dirname, './resources')
+const Contenedor = require('./resources/Contenedor.js')
+
+const app = express()
+
+const port = 8080
+
+const productos = new Contenedor(path.resolve(resourcesPath, './productos.txt'))
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+app.get('/productos', (req, res) => {
+  productos.getAll().then((prod) => {
+    res.send(JSON.stringify(prod, null, '\t')) //Se ve "feo"
+  })
+})
+
+app.get('/productoRandom', (req, res) => {
+  const randInt = getRandomInt(1, productos.objects.length + 1)
+  productos.getById(randInt).then((prod) => {
+    res.send(JSON.stringify(prod, null, '\t'))
+  })
+})
+
+const server = app.listen(port, () =>
+  console.log(`Server listening on port: ${server.address().port}`),
+)
+
+server.on('error', () => {
+  console.log('Hubo un error')
+})
